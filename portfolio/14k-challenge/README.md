83c414b1bbe9adcdd22b30137852f001cf154ac8
Hairy Blue <pecision.nickimarty@gmail.com>
2025-08-06 12:55:39 +0800

    14kB challenge

---
+++ portfolio/14k-challenge/README.md
@@ -0,0 +1,7 @@
+# MY PORTFOLIO WEBSITE
+> **NOTE!!!**
+>
+> To be build with next js (app router)
+
+Figma Design
+![HOME](assets/portfolio/Home.png)
---
+++ portfolio/14k-challenge/vite.config.js
@@ -0,0 +1,28 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+// import { viteSingleFile } from 'vite-plugin-singlefile'
+import simpleHtmlPlugin from 'vite-plugin-simple-html';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+export default defineConfig({
+//   plugins: [viteSingleFile()],
+  plugins: [simpleHtmlPlugin({
+    minify: true,
+  })],
+  root: resolve(__dirname, "src"),
+  build: {
+   emptyOutDir: true,
+   outDir: resolve(__dirname, "dist"),
+   //  minify: "esbuild",
+    cssCodeSplit: false,
+   //  assetsInlineLimit: 100_000_000,
+    rollupOptions: {
+      inlineDynamicImports: true,
+      input: {
+         main: resolve(__dirname, 'src/index.html'),
+         // home: resolve(__dirname, 'src/home/index.html'),
+      }
+    }
+  }
+})
