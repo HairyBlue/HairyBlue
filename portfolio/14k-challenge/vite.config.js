2ff52a65bd613aa78088a7655d3c3064eaf2007f
Hairy Blue <pecision.nickimarty@gmail.com>
2025-10-14 19:10:18 +0800

    update portfolio

--- portfolio/14k-challenge/vite.config.js
+++ portfolio/14k-challenge/vite.config.js
@@ -1,32 +1,41 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 // import { viteSingleFile } from 'vite-plugin-singlefile'
 import simpleHtmlPlugin from 'vite-plugin-simple-html';
 // import viteCompression from 'vite-plugin-compression';
-const __dirname = dirname(fileURLToPath(import.meta.url))
+// import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
-//   plugins: [viteSingleFile()],
+  //   plugins: [viteSingleFile()],
   plugins: [
     simpleHtmlPlugin({
       minify: true,
     }),
     // viteCompression()
   ],
-  root: resolve(__dirname, "src"),
+  root: '.',
   build: {
-   emptyOutDir: true,
-   outDir: resolve(__dirname, "dist"),
-   assetsDir: '', 
-   //  minify: "esbuild",
+    emptyOutDir: true,
+    outDir: resolve(__dirname, 'dist'),
+    assetsDir: 'assets',
+    minify: 'esbuild',
     cssCodeSplit: false,
-   //  assetsInlineLimit: 100_000_000,
+    // assetsInlineLimit: 100_000_000,
     rollupOptions: {
-      inlineDynamicImports: true,
+      // inlineDynamicImports: true,
+      // preserveEntrySignatures: false,
       input: {
-         main: resolve(__dirname, 'src/index.html'),
-         // home: resolve(__dirname, 'src/home/index.html'),
-      }
-    }
-  }
-})
+        index: resolve(__dirname, 'index.html'),
+        techs: resolve(__dirname, 'pages/techs.html'),
+        robots: resolve(__dirname, 'robots.txt'),
+      },
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+});
